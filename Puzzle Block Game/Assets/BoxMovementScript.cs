6f47aaff7c4a8273d851acf050508fa159d5ce26
StuffGames <44318828+StuffGames@users.noreply.github.com>
2019-04-01 08:50:26 -0400

    Update 1.0
    This is just the start of the project with all the files added to the repository. And then a creation of some elements but not much in depth creation. just the borders and the box as well as colliders. And some movement script.

---
+++ Puzzle Block Game/Assets/BoxMovementScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxMovementScript : MonoBehaviour
+{
+
+    Transform playerT;
+
+    private Vector3 pos;
+    private float speed = 0.125f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerT = transform;
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D) && playerT.position == pos)
+        {
+            pos += Vector3.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && playerT.position == pos)
+        {
+            pos += Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && playerT.position == pos)
+        {
+            pos += Vector3.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && playerT.position == pos)
+        {
+            pos += Vector3.down;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+}
