31b034d59d265703aa45ac0942e726e428bc5fca
StuffGames <44318828+StuffGames@users.noreply.github.com>
2019-04-03 11:00:32 -0400

    Update 1.1
    I am just working on colliders i just need to a lot more code to make it work. Also a change in types of colliders and amount.

--- Puzzle Block Game/Assets/BoxMovementScript.cs
+++ Puzzle Block Game/Assets/BoxMovementScript.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class BoxMovementScript : MonoBehaviour
 {
+    //public List<CircleCollider2D> playerColliders = new List<CircleCollider2D>();
+    public CircleCollider2D[] playerColliders = new CircleCollider2D[4];
 
     Transform playerT;
 
+    public BoxCollider2D wallCollider;
     private Vector3 pos;
     private Vector3 velTemp = Vector3.zero;
     public float speed = 0.125f;
     private bool buttonActive = false;
+    private bool isTouchingTop = false;
+    private bool isTouchingRight = false;
+    private bool isTouchingBottom = false;
+    private bool isTouchingLeft = false;
+
+    private CircleCollider2D topCollider;
+    private CircleCollider2D rightCollider;
+    private CircleCollider2D bottomCollider;
+    private CircleCollider2D leftCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         playerT = transform;
         pos = transform.position;
+
+        playerColliders = GetComponents<CircleCollider2D>();
+
+        int count = 0;
+
+        foreach (CircleCollider2D col in playerColliders)
+        {
+            count++;
+            //Debug.Log(count + " " + col.offset);
+        }
+
+        topCollider = playerColliders[0];
+        rightCollider = playerColliders[1];
+        bottomCollider = playerColliders[2];
+        leftCollider = playerColliders[3];
     }
 
     // Update is called once per frame
@@ -25,28 +52,29 @@ public class BoxMovementScript : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        if (moveX == 0)
+        /*if (moveX == 0)
         {
             buttonActive = false;
-        }
+        }*/
 
-        /*if (Input.GetKeyDown(KeyCode.D) && playerT.position == pos)
+        if (Input.GetKeyDown(KeyCode.D) && playerT.position == pos && isTouchingRight == false)
         {
             pos += Vector3.right;
         }
-        else if (Input.GetKeyDown(KeyCode.A) && playerT.position == pos)
+        else if (Input.GetKeyDown(KeyCode.A) && playerT.position == pos && isTouchingBottom == false)
         {
             pos += Vector3.left;
         }
-        else if (Input.GetKeyDown(KeyCode.W) && playerT.position == pos)
+        else if (Input.GetKeyDown(KeyCode.W) && playerT.position == pos && isTouchingTop == false)
         {
             pos += Vector3.up;
         }
-        else if (Input.GetKeyDown(KeyCode.S) && playerT.position == pos)
+        else if (Input.GetKeyDown(KeyCode.S) && playerT.position == pos && isTouchingLeft == false)
         {
             pos += Vector3.down;
-        }*/
-        if (moveX == 1 && playerT.position == pos)
+        }
+
+        /*if (moveX == 1 && playerT.position == pos)
         {
             if (buttonActive == false)
             {
@@ -61,10 +89,19 @@ public class BoxMovementScript : MonoBehaviour
                 buttonActive = true;
                 pos += new Vector3(moveX, 0);
             }
-        }
+        }*/
 
         transform.position = Vector3.SmoothDamp(playerT.position, pos, ref velTemp, speed);
 
         //transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
     }
+
+    private void FixedUpdate()
+    {
+        isTouchingTop = Physics2D.IsTouching(topCollider, wallCollider);
+        isTouchingRight = Physics2D.IsTouching(rightCollider, wallCollider);
+        isTouchingBottom = Physics2D.IsTouching(bottomCollider, wallCollider);
+        isTouchingLeft = Physics2D.IsTouching(leftCollider, wallCollider);
+    }
+
 }
